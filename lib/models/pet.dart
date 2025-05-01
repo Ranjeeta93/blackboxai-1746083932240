f7936c169853d81091ce653c53ff3a00c61e64d7
Ranjeeta93 <ranjeeta.betageri@cmr.edu.in>
2025-05-01 12:48:57 +0530

    Add lib/models/pet.dart 1746083936768

---
+++ lib/models/pet.dart
@@ -0,0 +1,19 @@
+class Pet {
+  final String id;
+  final String name;
+  final String breed;
+  final int age;
+  final double price;
+  final String imageUrl;
+  final String description;
+
+  Pet({
+    required this.id,
+    required this.name,
+    required this.breed,
+    required this.age,
+    required this.price,
+    required this.imageUrl,
+    required this.description,
+  });
+}
